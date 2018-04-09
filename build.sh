a31c1dc7483375e14ece399af3e5c1506d6729a1
Lukasz Stempniewicz <lukaszcorgi@gmail.com>
2018-04-09 14:58:27 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -x -e
 
 buildnumber=${4-$(date -u +"%y%m%d%H%M")}
 
-docker build -t "$1"/canary:1.0_"$buildnumber" -t "$1"/canary:latest_"$buildnumber" 1.0
+docker build --no-cache -t "$1"/canary:1.0_"$buildnumber" -t "$1"/canary:latest_"$buildnumber" 1.0
 
 docker login -u "$2" -p "$3"
 
